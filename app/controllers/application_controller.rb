8773f6524a2648050e62cf730e936457964bf237
Sergey Gulidov <S.Gulidov@gmail.com>
2013-01-14 16:11:24 +0200

    Add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+
+  private
+
+  	def current_cart
+  		Cart.find(session[:cart_id]) 	# выдал ли браузер сессию
+  	rescue ActiveRecord::RecordNotFound
+  		cart = Cart.create 				# если нет создаем новую корзину
+  		session[:cart_id] = cart.id 	# создаем сессию == id таблицы
+  		cart 							# возврящаем корзину
+  	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10 }
@@ -7,4 +11,16 @@ class Product < ActiveRecord::Base
   		with: %r{\.(gif|jpg|png)$}i,
   		message: 'Jpg, gif or png only.'
   }
+
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+  	if line_items.empty?
+  		return true
+  	else
+  		errors.add(:base, 'product exist in line_items cart')
+  		return false
+  	end
+  end
 end
